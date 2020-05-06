5ad801aa1b74cea694b0d94d2cfd4292b56458f0
banna1990 <banna19900501@gmail.com>
2020-05-06 20:21:44 +0800

    demo sample commit
    Signed-off-by: banna1990 <banna19900501@gmail.com>

---
+++ sample/src/com/roocon/test1/Main.java
@@ -0,0 +1,14 @@
+package com.roocon.test1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		List<Demo> demoList = new ArrayList<Demo>();
+		while(true) {
+			demoList.add(new Demo());
+		}
+	}
+
+}
