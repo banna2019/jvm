8e1de3872f0997bb6f60b8b5affdf5a668711837
banna1990 <banna19900501@gmail.com>
2020-05-07 15:09:48 +0800

    lambda update commit
    Signed-off-by: banna1990 <banna19900501@gmail.com>

---
+++ sample/src/com/roocon/test3/Main.java
@@ -0,0 +1,35 @@
+package com.roocon.test3;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+@SuppressWarnings("serial")
+public class Main extends JFrame {
+	
+	private JButton jb;
+	
+	public Main() {
+		this.setBounds(200, 200, 400, 200);
+		this.setTitle("lambda测试");
+		
+		jb = new JButton("click");
+//		jb.addActionListener(new ActionListener() {
+//			
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				System.out.println("clicked");
+//				
+//			}
+//		});
+		
+		jb.addActionListener(Event -> System.out.println("Hello"));
+		
+		this.add(jb);
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+
+}
