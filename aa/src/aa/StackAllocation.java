b550cbec15c85500bf5cea7e8cf9a4f7dd549fef
banna1990 <banna19900501@gmail.com>
2020-05-10 21:47:25 +0800

    update 逃逸分析与栈上分配

---
+++ aa/src/aa/StackAllocation.java
@@ -0,0 +1,31 @@
+package aa;
+
+public class StackAllocation {
+	
+	public StackAllocation obj;
+	
+	/**方法返回StackAllocation对象发生逃逸*/
+	public StackAllocation getInstance() {
+		
+		return obj == null? new StackAllocation():obj;
+	}
+
+	/**为成员属性赋值,发生逃逸*/
+	public void setObj() {
+		this.obj = new StackAllocation();
+	}
+
+	/**对象的作用域仅在当前方法中生效,没有发生逃逸*/
+	public void useStackAllocation() {
+		StackAllocation s = new StackAllocation();
+		
+	}
+	
+	/**引用成员变量的值,发生逃逸*/
+	public void useStackAllocation2() {
+		
+		StackAllocation s = getInstance();
+		
+	}
+	
+}
