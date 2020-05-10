a4d69b6c96423a1ff9b9b612c396f620384a4196
banna1990 <banna19900501@gmail.com>
2020-05-10 22:33:20 +0800

    update  jps commit

---
+++ sample/src/com/roocon/test10/Main.java
@@ -0,0 +1,17 @@
+package com.roocon.test10;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public Main() {
+		// TODO 自动生成的构造函数存根
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		sc.next();
+	}
+		
+}
