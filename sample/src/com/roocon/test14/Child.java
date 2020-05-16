f4eede2266ff903e08fb5dfa4fe23cee3791187e
banna1990 <banna19900501@gmail.com>
2020-05-16 23:50:34 +0800

    类初始化 update commit

---
+++ sample/src/com/roocon/test14/Child.java
@@ -0,0 +1,17 @@
+package com.roocon.test14;
+
+public class Child extends Parent {
+	
+	static {
+		
+		System.out.println("child 初始化...");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
---
+++ sample/src/com/roocon/test14/Parent.java
@@ -0,0 +1,9 @@
+package com.roocon.test14;
+
+public class Parent {
+	
+	static {
+		System.out.println("parent 初始化了...");
+	}
+
+}
