8d2809cc16ab5c11b16c2cc1958b0ba32b8a2d4b
banna1990 <banna19900501@gmail.com>
2020-05-18 00:13:44 +0800

    java lang update commit

---
+++ sample/src/java/lang/Object.java
@@ -0,0 +1,13 @@
+package java.lang;
+
+import com.sun.media.jfxmedia.events.NewFrameEvent;
+
+public class Object {
+	
+	public static void main(String[] args) {
+		
+		Object obj = new Object();
+		System.out.println("Hello");
+	}
+
+}
